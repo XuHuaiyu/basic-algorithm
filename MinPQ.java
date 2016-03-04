38eb71a9918f7a0b88c1c6233584dfbcebae77ed
xuhuaiyu <xhy4258685@gmail.com>
2016-03-04 09:20:59 +0800

    MinPQ(int maxSize)中,数组空间应申请maxSize+1大小,而不是maxSize大小

--- MinPQ.java
+++ MinPQ.java
@@ -17,7 +17,7 @@ public class MinPQ{
 
   public MinPQ(int maxSize){
     this.maxSize = maxSize;
-    elements = new int[maxSize];
+    elements = new int[maxSize+1];//注意，此处应当是maxSize+1的空间，需留出一个sentinel
     size = 0;
     elements[0] = MIN_VALUE;
   }
@@ -36,7 +36,7 @@ public class MinPQ{
     }
   }
 
-  public void percolateDown(int i){
+  public void percolateDown(int i){//下滤以下标作为判断条件，是因为子节点有两个，下滤一层后还需判断下子节点之间的大小
     int child;
     for(; i*2 <= size ; i = child){
       child = i * 2;
@@ -55,13 +55,13 @@ public class MinPQ{
     return elements[size+1];
   }
 
-  public void insert(int v){
+  public void insert(int v){ //插入操作以元素值作为循环判断条件，是因为插入是上浮操作，父节点只有一个
     if(isFull()){
       return;
     }
     int i;
-    for(i = ++size; elements[i/2] > v; i *= 2){
-        elements[i] = elements[i/2];
+    for(i = ++size; elements[i/2] > v; i /= 2){//此处判断条件应当是>v,而非>elements[i]
+      elements[i] = elements[i/2];
     }
     elements[i] = v;
   }
@@ -87,10 +87,20 @@ public class MinPQ{
   }
 
   public static void main(String[] args) {
-    MinPQ pq = new MinPQ(new int[]{150, 80, 40, 30, 10, 70, 110, 100, 20, 90, 60, 50, 120, 140, 130});
+    /*MinPQ pq = new MinPQ(new int[]{150, 80, 40, 30, 10, 70, 110, 100, 20, 90, 60, 50, 120, 140, 130});
+    // pq.insert(5);
     while(pq.getSize() > 0){
       System.out.println(pq.deleteMin());
+    }*/
+
+    MinPQ pq2 = new MinPQ(5);
+    for(int i = 5; i >= 1; i--){
+      pq2.insert(i);
     }
+    while(pq2.getSize() > 0){
+      System.out.println(pq2.deleteMin());
+    }
+
   }
 
 }
